4b80c298f417e7b963d25dc1be7ad5bf3fa3ef09
djfm <fm.de.jouvencel@gmail.com>
2015-11-16 00:40:20 +0100

    Split responsibilities of too big StreamBuilder class

--- test/streamBuilder.js
+++ test/streamBuilder.js
@@ -2,48 +2,46 @@ import chai from 'chai';
 chai.should();
 /* global describe, it */
 
-import streamBuilder from '../lib/streamBuilder';
+import streamReader from '../lib/streamReader';
+import streamWriter from '../lib/streamWriter';
 import {Node} from '../lib/node';
 
-describe('A StreamBuilder', function () {
+describe('A StreamReader', function () {
 
     it('should create nodes from values', function () {
-        const sb = streamBuilder();
-        sb.push("a");
-        sb.getNode(0).should.be.an.instanceof(Node);
+        const writer = streamWriter(streamReader());
+        writer.push("a");
+        writer.getNode(0).should.be.an.instanceof(Node);
     });
 
     it('should count nodes', function () {
-        const sb = streamBuilder();
-        sb.push("a");
-        sb.nodeCount().should.equal(1);
+        const writer = streamReader("a");
+        writer.nodeCount().should.equal(1);
     });
 
     describe('produces a stream that', function () {
         it('can be consumed value by value', function () {
-            const sb = streamBuilder();
-            sb.push("a");
-            sb.takeOne().should.equal("a");
-            sb.eos().should.equal(true);
+            const writer = streamReader("a");
+            writer.takeOne().should.equal("a");
+            writer.eos().should.equal(true);
         });
         it('can be peeked at without changing position', function () {
-            const sb = streamBuilder();
-            sb.push("a");
-            sb.peekOne().should.equal("a");
-            sb.eos().should.equal(false);
+            const writer = streamReader("a");
+            writer.peekOne().should.equal("a");
+            writer.eos().should.equal(false);
         });
     });
 
     describe('built from a string', function () {
         it('should accept a string as input and split it into char nodes', function () {
-            const sb = streamBuilder("hello");
-            sb.nodeCount().should.equal(5);
+            const writer = streamReader("hello");
+            writer.nodeCount().should.equal(5);
         });
 
         it('should keep track of line and column information', function () {
-            const sb = streamBuilder("h\nel\n\nlo");
+            const writer = streamReader("h\nel\n\nlo");
 
-            sb.getNode(0).getLocation().should.deep.equal({
+            writer.getNode(0).getLocation().should.deep.equal({
                 row: 1,
                 col: 1,
                 endRow: 1,
@@ -51,7 +49,7 @@ describe('A StreamBuilder', function () {
                 len: 1
             });
 
-            sb.getNode(1).getLocation().should.deep.equal({
+            writer.getNode(1).getLocation().should.deep.equal({
                 row: 1,
                 col: 2,
                 endRow: 1,
@@ -59,7 +57,7 @@ describe('A StreamBuilder', function () {
                 len: 1
             });
 
-            sb.getNode(2).getLocation().should.deep.equal({
+            writer.getNode(2).getLocation().should.deep.equal({
                 row: 2,
                 col: 1,
                 endRow: 2,
@@ -67,7 +65,7 @@ describe('A StreamBuilder', function () {
                 len: 1
             });
 
-            sb.getNode(7).getLocation().should.deep.equal({
+            writer.getNode(7).getLocation().should.deep.equal({
                 row: 4,
                 col: 2,
                 endRow: 4,
@@ -76,37 +74,37 @@ describe('A StreamBuilder', function () {
             });
         });
     });
+});
 
-    describe('linked with a source stream', function () {
-        it('should automatically build the node hierarchy according to nodes taken', function () {
-            const srcSb = streamBuilder("ab");
-            const sb = streamBuilder().setSourceStream(srcSb);
-            srcSb.takeOne();
-            srcSb.takeOne();
-            sb.push("AB");
-            sb.getNode(0).getChildren().length.should.equal(2);
+describe('A StreamWriter', function () {
+    it('should automatically build the node hierarchy according to nodes taken', function () {
+        const reader = streamReader("ab");
+        const writer = streamWriter(reader);
+        reader.takeOne();
+        reader.takeOne();
+        writer.push("AB");
+        writer.getNode(0).getChildren().length.should.equal(2);
+    });
+    it('should build a node hierarchy that keeps track of node locations', function () {
+        const reader = streamReader("ab");
+        const writer = streamWriter(reader);
+        reader.takeOne();
+        reader.takeOne();
+        writer.push("AB");
+        writer.getNode(0).getLocation().should.deep.equal({
+            row: 1,
+            col: 1,
+            endRow: 1,
+            endCol: 2,
+            len: 2
         });
-        it('should build a node hierarchy that keeps track of node locations', function () {
-            const srcSb = streamBuilder("ab");
-            const sb = streamBuilder().setSourceStream(srcSb);
-            srcSb.takeOne();
-            srcSb.takeOne();
-            sb.push("AB");
-            sb.getNode(0).getLocation().should.deep.equal({
-                row: 1,
-                col: 1,
-                endRow: 1,
-                endCol: 2,
-                len: 2
-            });
-        });
-        it('should build a node hierarchy that keeps track of source code', function () {
-            const srcSb = streamBuilder("ab");
-            const sb = streamBuilder().setSourceStream(srcSb);
-            srcSb.takeOne();
-            srcSb.takeOne();
-            sb.push("AB");
-            sb.getNode(0).getSource().should.equal("ab");
-        });
+    });
+    it('should build a node hierarchy that keeps track of source code', function () {
+        const reader = streamReader("ab");
+        const writer = streamWriter(reader);
+        reader.takeOne();
+        reader.takeOne();
+        writer.push("AB");
+        writer.getNode(0).getSource().should.equal("ab");
     });
 });
