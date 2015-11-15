e53fe2895e1846a2b1e6d84c8079465f011fa50e
djfm <fm.de.jouvencel@gmail.com>
2015-11-15 10:43:39 +0100

    String parser

--- test/parsers.js
+++ test/parsers.js
@@ -5,6 +5,7 @@ chai.should();
 import parserRunner     from '../lib/parserRunner';
 import whitespaceParser from '../lib/parsers/whitespace';
 import commentParser    from '../lib/parsers/comment';
+import stringParser    from '../lib/parsers/string';
 
 describe('The Parsers', function () {
     describe('A Whitespace Parser', function () {
@@ -54,6 +55,47 @@ describe('The Parsers', function () {
             stream.getNode(0).getValue().should.equal("hello");
             stream.getNode(0).getSource().should.equal(";hello\n");
             stream.getNode(1).getSource().should.equal("w");
+        });
+    });
+
+    describe('A String Parser', function () {
+        it('should parse a simple string', function () {
+            const runner = parserRunner(`"hello"`);
+            runner.run(stringParser);
+            const stream = runner.getStream();
+            stream.eos().should.equal(true, "stream should be at eos()");
+            stream.getNode(0).getValue().should.equal("hello");
+            stream.getNode(0).getSource().should.equal(`"hello"`);
+        });
+        it('should parse a string with an escaped quote', function () {
+            const input = `"hel\\\"lo"`;
+            input.length.should.equal(9, "Test string probably has the wrong number of escape chars.");
+            const runner = parserRunner(input);
+            runner.run(stringParser);
+            const stream = runner.getStream();
+            stream.eos().should.equal(true, "stream should be at eos()");
+            stream.getNode(0).getValue().should.equal("hel\"lo");
+            stream.getNode(0).getSource().should.equal(input);
+        });
+        it('should not ignore an escape char in front of a non-quote', function () {
+            const input = `"h\\i"`;
+            const runner = parserRunner(input);
+            runner.run(stringParser);
+            const stream = runner.getStream();
+            stream.eos().should.equal(true, "stream should be at eos()");
+            stream.getNode(0).getValue().should.equal("h\\i");
+            stream.getNode(0).getSource().should.equal(input);
+        });
+        it('should produce an error if string is unterminated', function () {
+            const input = `"hi`;
+            const runner = parserRunner(input);
+            const error = runner.run(stringParser);
+            const stream = runner.getStream();
+            error.message.should.equal('Unterminated string.');
+            error.row.should.equal(1);
+            error.col.should.equal(3);
+            stream.getPos().should.equal(0, "A failed parse should not advance the stream.");
+            stream.nodeCount().should.equal(3, "A failed parse should not change the stream.");
         });
     });
 });
