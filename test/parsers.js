5eb3cedb1876a1756a9c13d9d1d6c0677c5cd669
djfm <fm.de.jouvencel@gmail.com>
2015-11-15 10:06:02 +0100

    // simple comments parser

--- test/parsers.js
+++ test/parsers.js
@@ -2,8 +2,9 @@ import chai from 'chai';
 chai.should();
 /* global describe, it */
 
-import parserRunner from '../lib/parserRunner';
+import parserRunner     from '../lib/parserRunner';
 import whitespaceParser from '../lib/parsers/whitespace';
+import commentParser    from '../lib/parsers/comment';
 
 describe('The Parsers', function () {
     describe('A Whitespace Parser', function () {
@@ -33,6 +34,26 @@ describe('The Parsers', function () {
             error.message.should.equal(`Expected a whitespace character, but found "a".`);
             error.row.should.equal(1);
             error.col.should.equal(1);
+        });
+    });
+
+    describe('A Comment Parser', function () {
+        it('should parse lines starting with a semi-colon', function () {
+            const runner = parserRunner(";hello");
+            runner.run(commentParser);
+            const stream = runner.getStream();
+            stream.eos().should.equal(true, "stream should be at eos()");
+            stream.getNode(0).getValue().should.equal("hello");
+            stream.getNode(0).getSource().should.equal(";hello");
+        });
+        it('should stop after the first line break', function () {
+            const runner = parserRunner(";hello\nworld");
+            runner.run(commentParser);
+            const stream = runner.getStream();
+            stream.eos().should.equal(false, "stream should not be at eos()");
+            stream.getNode(0).getValue().should.equal("hello");
+            stream.getNode(0).getSource().should.equal(";hello\n");
+            stream.getNode(1).getSource().should.equal("w");
         });
     });
 });
