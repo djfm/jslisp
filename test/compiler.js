4ae1989bb7e82f006bec43aac2e45943b4d387cf
djfm <fm.de.jouvencel@gmail.com>
2015-11-22 12:26:59 +0100

    Constants and Strings

--- test/compiler.js
+++ test/compiler.js
@@ -11,6 +11,36 @@ describe('The JSLISP Compiler', function () {
             node.getSource().should.equal("hello");
             node.getTokenType().should.equal("identifier");
         });
+        it('should compile an integer constant', function () {
+            const node = compiler("123").getNode(0);
+            node.getSource().should.equal("123");
+            node.getValue().should.equal(123);
+            node.getTokenType().should.equal("constant");
+        });
+        it('should compile a floating point constant', function () {
+            const node = compiler("123.456").getNode(0);
+            node.getSource().should.equal("123.456");
+            node.getValue().should.equal(123.456);
+            node.getTokenType().should.equal("constant");
+        });
+        it('should compile the `false` constant', function () {
+            const node = compiler("false").getNode(0);
+            node.getSource().should.equal("false");
+            node.getValue().should.equal(false);
+            node.getTokenType().should.equal("constant");
+        });
+        it('should compile the `true` constant', function () {
+            const node = compiler("true").getNode(0);
+            node.getSource().should.equal("true");
+            node.getValue().should.equal(true);
+            node.getTokenType().should.equal("constant");
+        });
+        it('should compile a string', function () {
+            const node = compiler(`"hello"`).getNode(0);
+            node.getSource().should.equal(`"hello"`);
+            node.getValue().should.equal("hello");
+            node.getTokenType().should.equal("string");
+        });
         it('should compile a basic arithmetic operation', function () {
             compiler("(+ 1 2)");
         });
